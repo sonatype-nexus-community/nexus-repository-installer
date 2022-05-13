82c114ff97d627b04f8f2e83893648b748dd1792
Dan Rollo <danrollo@gmail.com>
2022-05-13 12:04:48 -0400

    fix SC2086 in run-alien.sh

--- deb/run-alien.sh
+++ deb/run-alien.sh
@@ -8,7 +8,7 @@ export RPM_NAME=$2
 echo "RPM_NAME: ${RPM_NAME}"
 
 # split up running of alien program to allow us to inject rpm requires into deb depends stanza of debian control file
-alien --generate --to-deb --scripts /data/build/${RPM_NAME}
+alien --generate --to-deb --scripts "/data/build/${RPM_NAME}"
 #cat "${RPM_BASENAME}/debian/control"
 sed -i -e 's/Depends: \${shlibs:Depends}/Depends: \${shlibs:Depends}, openjdk-8-jdk-headless | java8-sdk-headless/' "${RPM_BASENAME}/debian/control"
 #cat "${RPM_BASENAME}/debian/control"
